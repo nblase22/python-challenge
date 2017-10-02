39203e96dbfe3976c3a99b41623cf16c4cd29bc2
nblase22 <nkmoveout@gmail.com>
2017-10-02 18:44:17 -0400

    Added the ssn fix and the date fix to PyBoss main.py

--- PyBoss/main.py
+++ PyBoss/main.py
@@ -2,6 +2,7 @@
 
 import os
 import csv
+import datetime
 
 #declare variables
 nameParse = []
@@ -12,13 +13,15 @@ state = []
 
 
 
-#import the first CSV file of employee data
+
+#import the csv files of employee data
 fileOne = "employee_data1.csv"
 fileTwo = "employee_data2.csv"
 
 empdataOne_csv = os.path.join('Resources', fileOne)
 empdataTwo_csv = os.path.join('Resources', fileTwo)
 
+#put the first file into a series of lists for manipulation
 with open(empdataOne_csv, newline="") as csvfile:
     next(csvfile, None)
     csvreader = csv.reader(csvfile, delimiter = ",")
@@ -30,7 +33,7 @@ with open(empdataOne_csv, newline="") as csvfile:
         ssn.append(row[3])
         state.append(row[4])
 
-
+# put the second file into a series of lists for manipulation
 with open(empdataTwo_csv, newline="") as csvfiletwo:
     next(csvfiletwo, None)
     csvreadertwo = csv.reader(csvfiletwo, delimiter = ",")
@@ -42,10 +45,30 @@ with open(empdataTwo_csv, newline="") as csvfiletwo:
         ssn.append(row[3])
         state.append(row[4])
 
-
+#parse the names into a big list
 name = [words for segments in nameParse for words in segments.split()]
 
+#grab every other  element for first name from name list starting at first item
 first_name = name[::2]
+
+#grab every other element for last name from name list starting at second item
 last_name = name[1::2]
+
+#convert dob from yyyy-mm-dd to dd/mm/yyyy
+for i in range(0, len(dob)):
+    dob[i] = datetime.datetime.strptime(dob[i], '%Y-%m-%d').strftime('%m/%d/%y')
+
+# * out the first five numerals in the ssn
+for j in range(0, len(ssn)):
+    ssnList = list(ssn[j])
+    for k in range(0,3):
+        ssnList[k] = '*'
+    for l in range(4,6):
+        ssnList[l] = '*'
+
+    ssn[j] = "".join(ssnList)
+
+
+
 
 
