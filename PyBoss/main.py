e8dc15afcaa4bb9fee892bd679c293b52ae8ae92
nblase22 <nkmoveout@gmail.com>
2017-10-02 17:49:55 -0400

    first update of PyBoss main.py

--- PyBoss/main.py
+++ PyBoss/main.py
@@ -12,10 +12,31 @@ state = []
 
 
 #import the first CSV file of employee data
-filename = input("Input the name of the data file: ")
+fileOne = "employee_data1.csv"
+fileTwo = "employee_data2.csv"
 
-empdata_csv = os.path.join('Resources', filename)
+empdataOne_csv = os.path.join('Resources', fileOne)
+empdataTwo_csv = os.path.join('Resources', fileTwo)
 
-with open(empdata_csv, newline="") as csvfile:
+with open(empdataOne_csv, newline="") as csvfile:
     next(csvfile, None)
     csvreader = csv.reader(csvfile, delimiter = ",")
+
+    for row in csvreader:
+        emp_id.append(row[0])
+        name.append(row[1])
+        dob.append(row[2])
+        ssn.append(row[3])
+        state.append(row[4])
+
+
+with open(empdataTwo_csv, newline="") as csvfiletwo:
+    next(csvfiletwo, None)
+    csvreadertwo = csv.reader(csvfiletwo, delimiter = ",")
+
+    for row in csvreadertwo:
+        emp_id.append(row[0])
+        name.append(row[1])
+        dob.append(row[2])
+        ssn.append(row[3])
+        state.append(row[4])
