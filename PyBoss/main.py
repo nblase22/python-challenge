285db4021cbf5ecf177b630a0b1a543028213fda
nblase22 <nkmoveout@gmail.com>
2017-10-02 18:23:45 -0400

    Updates PyBoss main.py to parse the csv and split the name list

--- PyBoss/main.py
+++ PyBoss/main.py
@@ -4,11 +4,12 @@ import os
 import csv
 
 #declare variables
-name = []
+nameParse = []
 dob = []
 emp_id = []
 ssn = []
 state = []
+
 
 
 #import the first CSV file of employee data
@@ -24,7 +25,7 @@ with open(empdataOne_csv, newline="") as csvfile:
 
     for row in csvreader:
         emp_id.append(row[0])
-        name.append(row[1])
+        nameParse.append(row[1])
         dob.append(row[2])
         ssn.append(row[3])
         state.append(row[4])
@@ -36,7 +37,15 @@ with open(empdataTwo_csv, newline="") as csvfiletwo:
 
     for row in csvreadertwo:
         emp_id.append(row[0])
-        name.append(row[1])
+        nameParse.append(row[1])
         dob.append(row[2])
         ssn.append(row[3])
         state.append(row[4])
+
+
+name = [words for segments in nameParse for words in segments.split()]
+
+first_name = name[::2]
+last_name = name[1::2]
+
+
