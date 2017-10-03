ddb26e3cc2ee755482f083ab74579203aee91262
nblase22 <nkmoveout@gmail.com>
2017-10-03 18:30:41 -0400

    Code improved to get word count, letter count, sentence count in PyParagraph main.py

--- PyParagraph/main.py
+++ PyParagraph/main.py
@@ -18,9 +18,27 @@ with open(paragraph_file, 'r') as txtFile:
 wordList = paragraph.split()
 wordCount = len(wordList)
 
-print(wordCount)
 
 
 # figure out number of sentences
 sentences = re.split(r' *[\.\?!][\'"\)\]]* *', paragraph)
-print(len(sentences))
+
+# get average letter count
+word_breakout = []
+for item in sentences:
+    word_breakout.append(item.split())
+
+flat_breakout = []
+for sublist in word_breakout:
+    for item in sublist:
+        flat_breakout.append(item)
+
+letterCount = 0
+for word in flat_breakout:
+    letterCount = letterCount + len(word)
+
+avg_letter = letterCount / wordCount
+
+
+
+# get average sentence length
